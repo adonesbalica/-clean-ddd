382c3814476ef24ecdb3bd0541aefea24ddb4ffb
Adones Balica <adonesbalica@gmail.com>
2026-05-17 09:32:17 -0300

    feat: implement notification system with send and read functionality

--- src/domain/notification/application/subscribers/on-answer-created.ts
+++ src/domain/notification/application/subscribers/on-answer-created.ts
@@ -1,9 +1,14 @@
 import { DomainEvents } from '@/core/events/domain-events'
 import { EventHandler } from '@/core/events/event-handler'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/question-repository'
 import { AnswerCreatedEvent } from '@/domain/forum/enterprise/events/answer-created-event'
+import { SendNotificationUseCase } from '../use-cases/send-notification'
 
 export class OnAnswerCreated implements EventHandler {
-  constructor() {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private sendNotification: SendNotificationUseCase
+  ) {
     this.setupSubscriptions()
   }
 
@@ -18,6 +23,18 @@ export class OnAnswerCreated implements EventHandler {
     answer,
     occurredAt,
   }: AnswerCreatedEvent) {
-    console.log(answer, occurredAt)
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString()
+    )
+
+    if (question) {
+      await this.sendNotification.execute({
+        recipientId: question.authorId.toString(),
+        title: `Nova resposta em ${question.title
+          .substring(0, 40)
+          .concat('...')}`,
+        content: answer.excerpt,
+      })
+    }
   }
 }
